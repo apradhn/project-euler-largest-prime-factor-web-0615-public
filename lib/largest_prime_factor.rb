0759406dd4d1dd10750049a9dc6ef63975f7ee78
apradhn <abhijit.pradhan6@gmail.com>
2015-07-09 00:08:05 -0400

    finally done

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,14 @@
-# Enter your procedural solution here!
+def largest_prime_factor(num)
+  prime = num
+  (Math.sqrt(num).round).downto(2) do |i|
+    if num % i == 0 && prime?(i)
+      prime = i
+      break
+    end
+  end
+  prime
+end
+
+def prime?(num)
+  (2..(Math.sqrt(num).round)).to_a.reverse.none?{|n| num % n == 0}
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,22 @@
-# Enter your object-oriented solution here!
+class LargestPrimeFactor
+  attr_reader :num
+
+  def initialize(num)
+    @num = num
+  end
+
+  def number
+    prime = num
+    (Math.sqrt(num).round).downto(2) do |i|
+      if num % i == 0 && prime?(i)
+        prime = i
+        break
+      end
+    end
+    prime    
+  end
+  
+  def prime?(num)
+    (2..(Math.sqrt(num).round)).to_a.reverse.none?{|n| num % n == 0}
+  end  
+end
